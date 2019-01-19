a0c6a45c95ff995971d5e7f45faa5b914755e73f
Jörg Riemenschneider <joerg.riemenschneider@gmail.com>
2019-01-19 17:30:49 +0100

    Correct exit status on text-environment.sh

--- test-environment.sh
+++ test-environment.sh
@@ -5,26 +5,31 @@ source .env
 export TDS_DOMAINNAME=$TDS_DOMAINNAME
 export TDS_VOLUMEDIR=$TDS_VOLUMEDIR
 
+STATUS=0
+
 echo "###### Are all dockers running?"
 if docker ps --filter="name=proxy" -q; then
   echo "Proxy is running!"
-  exit 0
+  $STATUS=0
 else
   echo "Proxy is NOT running."
-  exit 1
+$STATUS=1
 fi
-
-
-
-
 
 
 
 echo "###### Are all dockers reachable via http?"
 if curl -s http://proxy.$TDS_DOMAINNAME | grep -q '<a href="/dashboard/">Found</a>'; then
   echo "Proxy is reachable!"
-  exit 0
+  $STATUS=0
 else
   echo "Proxy is NOT reachable!"
-  exit 1
+  $STATUS=1
 fi
+
+
+
+
+
+
+exit $STATUS
