eea7e6b9477b09a39ab2549e92d5d276ad99da3a
Jörg Riemenschneider <joerg.riemenschneider@gmail.com>
2019-01-19 17:25:48 +0100

    make a general testrun script Update travis

--- test-environment.sh
+++ test-environment.sh
@@ -5,10 +5,26 @@ source .env
 export TDS_DOMAINNAME=$TDS_DOMAINNAME
 export TDS_VOLUMEDIR=$TDS_VOLUMEDIR
 
+echo "###### Are all dockers running?"
+if docker ps --filter="name=proxy" -q; then
+  echo "Proxy is running!"
+  exit 0
+else
+  echo "Proxy is NOT running."
+  exit 1
+fi
+
+
+
+
+
+
+
+echo "###### Are all dockers reachable via http?"
 if curl -s http://proxy.$TDS_DOMAINNAME | grep -q '<a href="/dashboard/">Found</a>'; then
   echo "Proxy is reachable!"
   exit 0
 else
-  echo "Tests failed!"
+  echo "Proxy is NOT reachable!"
   exit 1
 fi
