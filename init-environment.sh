0bd1f6ca43595185f9012353772bcae6cdba730e
Jörg Riemenschneider <joerg.riemenschneider@gmail.com>
2019-01-19 02:00:43 +0100

    Added sudo for file operations

--- init-environment.sh
+++ init-environment.sh
@@ -6,13 +6,13 @@ export TDS_DOMAINNAME=$TDS_DOMAINNAME
 export TDS_VOLUMEDIR=$TDS_VOLUMEDIR
 
 
-rm ./install.log && touch ./install.log
+touch ./install.log
 
 echo "######  1.Preparing environment..."
 
 echo "######  2.Creating persistent config storage in $TDS_VOLUMEDIR"
-mkdir --p $TDS_VOLUMEDIR/.config
-cp -r ./config/traefik $TDS_VOLUMEDIR/.config/
+sudo mkdir --p $TDS_VOLUMEDIR/.config
+sudo cp -r ./config/traefik $TDS_VOLUMEDIR/.config/
 
 
 echo "######  3.Create and start proxy container -> traefik"
