ef7949c07bb5d9b8cbebc978ee7a531dbb680fb0
Jörg Riemenschneider <joerg.riemenschneider@gmail.com>
2019-02-03 19:32:25 +0100

    Wait before test scripts so devshop has completed setup

--- tests/run-simple-tests.sh
+++ tests/run-simple-tests.sh
@@ -2,7 +2,7 @@
 
 echo "######  1.Preparing environment..."
 
-sleep 5
+sleep 1m
 source .env
 export TDS_BRANDING_LOGO_URL=$TDS_BRANDING_LOGO_URL
 export TDS_BRANDING_COMPANY_NAME=$TDS_BRANDING_COMPANY_NAME
