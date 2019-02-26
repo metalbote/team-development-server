2492e486348283d7223eb1bf251a4f730b82bd16
Jörg Riemenschneider <dermetalbote@gmx.net>
2019-02-26 13:09:45 +0100

    Add database backup routine open gitea port

--- tds-backup.sh
+++ tds-backup.sh
@@ -9,16 +9,13 @@
 
 # get day of the month
 DATE=`date +%d`
-source /usr/local/share/team-development-server/.env
+
 
 # directories to backup (use . for /)
 CONTAINERS="tds-drone tds-gitea tds-mailhog tds-portainer"
 BDIRS="var/lib/docker/volumes/team-development-server_tds-gitea-data/ var/lib/docker/volumes/team-development-server_tds-portainer-data/"
 TDIR="var/backup/volumes"
-DBDIR="var/backup/databases"
 LOGDIR='/var/backup/log'
-DB_CONTAINERS="tds_giteadb"
-TIMESTAMP=$(date +"%F")
 
 
 mkdir -p /$LOGDIR
@@ -48,18 +45,5 @@ do
   CMD="duplicity $TYPE -v5 --no-encryption $EXCLUDE /var/lib/docker/volumes/team-development-server_$CONTAINER-data/ file:///$TDIR/$CONTAINER >> $LOGDIR/$CONTAINER.log"
   eval  $CMD
 
-done
-
-
-for DB_CONTAINER in $DB_CONTAINERS
-do
-  mkdir -p /$DBDIR/$DB_CONTAINER/$TIMESTAMP
-
-  databases=`docker exec $DB_CONTAINER sh -c 'exec mysql --user=root -p$TDS_MYSQL_ROOT_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"'`
-
-  for db in $databases; do
-    docker exec $DB_CONTAINER /usr/bin/mysqldump -u root --password=$TDS_MYSQL_ROOT_PASSWORD $db | gzip >  /$DBDIR/$DB_CONTAINER/$TIMESTAMP/$db.sql.gz
-  done
-  find /$DBDIR/$DB_CONTAINER -maxdepth 1 -type d -mtime +30 -exec rm -rf {} +
 done
 exit 0
